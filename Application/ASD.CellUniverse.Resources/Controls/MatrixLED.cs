2c1e5e7a9651b412c4d8a78141be677cea98ad33
Stanislav Kuzmich [Art.Stea1th] <art.stea1th@gmail.com>
2017-05-06 16:26:13 +0300

    #17 experimental: Fade with DoubleAnimation - slow on 60+ layers, with 120+ opacity masks

--- Application/ASD.CellUniverse.Resources/Controls/MatrixLED.cs
+++ Application/ASD.CellUniverse.Resources/Controls/MatrixLED.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 
 namespace ASD.CellUniverse.Resources.Controls {
@@ -19,7 +22,7 @@ namespace ASD.CellUniverse.Resources.Controls {
 
         public Brush Background { get => (Brush)GetValue(BackgroundProperty); set => SetValue(BackgroundProperty, value); }
         public Brush Foreground { get => (Brush)GetValue(ForegroundProperty); set => SetValue(ForegroundProperty, value); }
-
+        public bool ShowFade { get => (bool)GetValue(ShowFadeProperty); set => SetValue(ShowFadeProperty, value); }
 
         public static readonly DependencyProperty SourceProperty = DependencyProperty.Register(nameof(
             Source), typeof(uint[,]), typeof(MatrixLED), new FrameworkPropertyMetadata(
@@ -32,6 +35,10 @@ namespace ASD.CellUniverse.Resources.Controls {
         public static readonly DependencyProperty ForegroundProperty =
             TextElement.ForegroundProperty.AddOwner(typeof(MatrixLED), new FrameworkPropertyMetadata(
                 SystemColors.ControlTextBrush, FrameworkPropertyMetadataOptions.Inherits | FrameworkPropertyMetadataOptions.SubPropertiesDoNotAffectRender));
+
+        public static readonly DependencyProperty ShowFadeProperty = DependencyProperty.Register(nameof(
+            ShowFade), typeof(bool), typeof(MatrixLED), new FrameworkPropertyMetadata(
+                false));
 
         static MatrixLED() => StyleProperty.OverrideMetadata(typeof(MatrixLED), new FrameworkPropertyMetadata(CreateStyle()));
 
@@ -59,21 +66,90 @@ namespace ASD.CellUniverse.Resources.Controls {
             return MeasureArrangeHelper.ComputeSize(arrangeSize, contentSize);
         }
 
+        private FadeHelper fh;
+
         protected override void OnRender(DrawingContext dc) {
 
             RepaintLedsMask();
 
             dc.DrawRectangle(Background, null, new Rect(new Point(), RenderSize));
+
+            // bad - code of fade using =)
+            if (ShowFade) {
+                if (fh == null) {
+                    fh = new FadeHelper(1000, 60, Foreground, RenderSize);
+                }
+                if (fh.RenderSize != RenderSize) {
+                    fh.RenderSize = RenderSize;
+                }
+
+                fh.Redraw(dc);
+                fh.AddImage(mask.Clone());
+            }
+
             dc.PushOpacityMask(new ImageBrush(mask));
             dc.DrawRectangle(Foreground, null, new Rect(new Point(), RenderSize));
         }
 
         private void RepaintLedsMask() {
             mask = BitmapHelper.Valid(mask, contentSize);
-            using (var context = new WriteableContext(mask)) {
+            if (Source != null && Source.Length > 0) {
+                using (var context = new WriteableContext(mask)) {
 
-                if (cellSize == 1) { context.WritePixels(Source); }
-                else { context.WriteCells(Source, cellSize); }
+                    if (cellSize == 1) { context.WritePixels(Source); }
+                    else { context.WriteCells(Source, cellSize); }
+                }
+            }
+        }
+
+        // bad-impl.fade
+        private class FadeHelper {
+
+            private int historySize;
+            private Brush brush;
+            public Size RenderSize { get; set; }
+
+
+            private TimeSpan time;
+            private Queue<ImageSource> bitmaps;
+
+            private DoubleAnimation fadeAnimation;
+
+            public void AddImage(ImageSource image) {
+                bitmaps.Enqueue(image);
+                if (bitmaps.Count > historySize) {
+                    bitmaps.Dequeue();
+                }
+            }
+
+            public void Redraw(DrawingContext dc) {
+
+                dc.PushOpacity(0.0, fadeAnimation.CreateClock());
+
+                for (var i = 0; i < bitmaps.Count; ++i) {
+                    dc.PushOpacityMask(new ImageBrush(bitmaps.ElementAt(i)));
+                    dc.PushOpacity((1.0 / historySize)/* * (i + 1)*/);
+
+                    dc.DrawRectangle(brush, null, new Rect(new Point(), RenderSize));
+
+                    dc.Pop();
+                    dc.Pop();
+                }
+                dc.Pop();
+            }
+
+            public FadeHelper(int durationMilliseconds, int historySize, Brush brush, Size renderSize) {
+
+                this.historySize = historySize;
+                this.brush = brush;
+                this.RenderSize = renderSize;
+
+                // ----
+
+                bitmaps = new Queue<ImageSource>(historySize);
+
+                time = TimeSpan.FromMilliseconds(durationMilliseconds);
+                fadeAnimation = new DoubleAnimation(1.0, 0.0, time);
             }
         }
     }
